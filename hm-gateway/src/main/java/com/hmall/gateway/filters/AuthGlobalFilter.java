6c13911ed159546c59881fed86f1b26c4c8095e6
xzy <2894702202@qq.com>
2024-06-19 16:58:56 +0800

    feat:登录校验

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFilter.java
@@ -0,0 +1,87 @@
+package com.hmall.gateway.filters;
+
+
+import com.hmall.common.exception.UnauthorizedException;
+import com.hmall.gateway.config.AuthProperties;
+import com.hmall.gateway.utils.JwtTool;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class AuthGlobalFilter implements GlobalFilter, Ordered {
+
+    private final AuthProperties authProperties;
+
+    private final JwtTool jwtTool;
+
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1.获取request
+        ServerHttpRequest request = exchange.getRequest();
+        // 2.判断是否需要做登录拦截
+        String path = request.getPath().toString();
+        if (isExclude(path)) {
+            // 放行
+            return chain.filter(exchange);
+        }
+
+        // 3.获取token
+        String token = "";
+        List<String> headers = request.getHeaders().get("authorization");
+        if (headers != null && !headers.isEmpty()) {
+            token = headers.get(0);
+        }
+
+        // 4.校验并解析token
+        Long userId = null;
+        try {
+            userId = jwtTool.parseToken(token);
+        } catch (UnauthorizedException e) {
+            // 如果报错，肯定是解析失败。则拦截，并设置状态码为401（未授权）
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();  // 终止
+        }
+        // todo 5.传递用户信息
+        System.out.println("userId = " + userId);
+        // 6.放行
+        return chain.filter(exchange);
+    }
+
+
+    /**
+     * 判断当前请求路径是否在排除的路径中
+     *
+     * @param path
+     * @return
+     */
+    private boolean isExclude(String path) {
+        for (String pathPattern : authProperties.getExcludePaths()) {
+            if(antPathMatcher.match(pathPattern, path)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/MyGlobalFilter.java
@@ -0,0 +1,31 @@
+package com.hmall.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class MyGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // todo 模拟登录校验逻辑
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders();
+        System.out.println("headers = " + headers);
+        // 放行
+        return chain.filter(exchange);
+    }
+
+
+    @Override
+    public int getOrder() {
+        // 过滤器执行顺序，值越小，优先级越高
+        return 0;
+    }
+}
